691f0f21e79ccc7b6a07ef0e5abbb706b0428da4
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2023-01-28 17:45:07 -0500

    Inicio maquetacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-store';
+  imgParent: string = '';
+  showImage = true;
+  products: Product[] = [{
+    id: '1',
+    name: 'EL mejor juguete',
+    price: 565,
+    image: './assets/images/toy.jpg'
+  },
+  {
+    id: '2',
+    name: 'Bicicleta casi nueva',
+    price: 356,
+    image: './assets/images/bike.webp'
+  },
+  {
+    id: '3',
+    name: 'Colleción de albumnes',
+    price: 34,
+    image: './assets/images/album.webp'
+  },
+  {
+    id: '4',
+    name: 'Mis libros',
+    price: 23,
+    image: './assets/images/books.jpg'
+  },
+  {
+    id: '5',
+    name: 'Gafas',
+    price: 50,
+    image: './assets/images/glasses.webp'
+  }];
+
+  onLoaded(img: string) {
+    console.log('log padre: ', img);
+  }
+
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
+
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+export class ImgComponent implements OnChanges, OnInit, AfterViewInit, OnDestroy {
+
+  img : string = '';
+
+  @Input('img') set changeImage(newImg: string) {
+    this.img = newImg;
+    console.log('change just image => ', this.img);
+    // code
+  }
+  @Input() alt : string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = '../assets/images/default.png';
+  counter = 0;
+  counterFn: number | undefined;
+
+  constructor() {
+    // before render, once, not async
+    console.log('constructor, ', 'imgValues => ', this.img);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // before and during render, changes on inputs, several times
+    console.log('ngOnChanges, ', 'imgValues => ', this.img);
+    console.log('changes', changes);
+
+  }
+
+  ngOnInit(): void {
+    // before render, async - fetch, once
+    console.log('ngOnInit, ', 'imgValues => ', this.img);
+    this.counterFn = window.setInterval(() => {
+      this.counter += 1;
+      console.log('run counter');
+
+    }, 1000);
+  }
+
+  ngAfterViewInit(): void {
+    // after render, handler children
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    // delete
+    console.log('ngOnDestroy');
+    window.clearInterval(this.counterFn);
+  }
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.log('log hijo');
+    this.loaded.emit(this.img);
+  }
 
 }
